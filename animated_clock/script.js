04dc1eaa1de780899262202f763717fdea747824
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-03-20 16:02:31 +0530

    Feat:Added hours & minute notation.

--- animated_clock/script.js
+++ animated_clock/script.js
@@ -21,4 +21,31 @@ ctx.lineWidth = 14;
 ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
 ctx.fill();
 ctx.stroke();
-ctx.retore();
+ctx.restore();
+
+// draw hour lines
+ctx.save();
+ctx.beginPath();
+for (let i = 0; i < 12; i++) {
+  ctx.strokeStyle = "#b3230c";
+  ctx.rotate(Math.PI / 6);
+  ctx.lineWidth = 6;
+  ctx.moveTo(100, 0);
+  ctx.lineTo(120, 0);
+  ctx.stroke();
+}
+ctx.restore();
+
+// Draw minutes hands
+ctx.save();
+ctx.lineWidth = 3;
+for (let i = 0; i < 60; i++) {
+  if (i % 5 !== 0) {
+    ctx.beginPath();
+    ctx.moveTo(110, 0);
+    ctx.lineTo(120, 0);
+    ctx.stroke();
+  }
+  ctx.rotate(Math.PI / 30);
+}
+ctx.restore();
