8c07055c5042e543c50101c95fb26e65b4406e26
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-03-25 21:40:35 +0530

    Feat:Play and pause the music.

---
+++ music-player/script.js
@@ -0,0 +1,42 @@
+const musicContainer = document.getElementById("music-container");
+const play = document.getElementById("play");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const imageCover = document.getElementById("cover");
+const icon = play.querySelector("i.fas").classList;
+
+const songs = ["hey", "summer", "ukulele"];
+let songIndex = 0;
+
+audio.src = `music/${songs[songIndex]}.mp3`;
+imageCover.src = `images/${songs[songIndex]}.jpg`;
+
+// Play music
+function songPlay() {
+  musicContainer.classList.add("play");
+  audio.play();
+  icon.remove("fa-play");
+  icon.add("fa-pause");
+}
+
+// pause music
+function songPause() {
+  musicContainer.classList.remove("play");
+  audio.pause();
+  icon.add("fa-play");
+  icon.remove("fa-pause");
+}
+
+// check for music Play And Pause
+function playMusic() {
+  const isPlaying = musicContainer.classList.contains("play");
+  if (isPlaying) {
+    songPause();
+  } else {
+    songPlay();
+  }
+}
+
+play.addEventListener("click", playMusic);
